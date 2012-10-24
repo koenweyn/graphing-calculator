5580af4a1fb52cf6149816b3a117ab4fb3ff50c9
Koen Weyn <koenweyn@blibo.be>
2012-10-24 11:08:10 +0200

    test for performOperation

--- CalculatorTests/CalculatorTests.m
+++ CalculatorTests/CalculatorTests.m
@@ -25,6 +25,35 @@
     [super tearDown];
 }
 
+- (void)testPerformOperation
+{
+    CalculatorBrain *brain = [[CalculatorBrain alloc]init];
+    [brain pushOperand:5.0];
+    [brain pushOperand:6.0];
+    double result = [brain performOperation:@"+"];
+    
+    STAssertEquals(result, 11.0, nil);
+    
+    [brain pushOperand:9.0];
+    result = [brain performOperation:@"*"];
+    
+    STAssertEquals(result, 99.0, nil);
+    
+    [brain pushOperand:11.0];
+    result = [brain performOperation:@"/"];
+    
+    STAssertEquals(result, 9.0, nil);
+    
+    [brain pushOperand:4];
+    result = [brain performOperation:@"sqrt"];
+
+    STAssertEquals(result, 2.0, nil);
+    
+    result = [brain performOperation:@"-"];
+    
+    STAssertEquals(result, 7.0, nil);
+}
+
 - (void)testDescriptionOfProgram
 {
     CalculatorBrain *brain = [[CalculatorBrain alloc]init];
@@ -34,12 +63,26 @@
     
     NSString *desc = [CalculatorBrain descriptionOfProgram:brain.program];
     STAssertEqualObjects(desc, @"5 + 6", nil);
-
+    
     [brain pushOperand:9.0];
     [brain performOperation:@"*"];
-
+    
     desc = [CalculatorBrain descriptionOfProgram:brain.program];
     STAssertEqualObjects(desc, @"(5 + 6) * 9", nil);
+    
+    [brain pushOperand:20.5];
+    [brain performOperation:@"/"];
+    
+    desc = [CalculatorBrain descriptionOfProgram:brain.program];
+    STAssertEqualObjects(desc, @"((5 + 6) * 9) / 20.5", nil);
+    
+    [brain pushOperand:2];
+    [brain performOperation:@"sqrt"];
+    [brain performOperation:@"-"];
+    
+    desc = [CalculatorBrain descriptionOfProgram:brain.program];
+    STAssertEqualObjects(desc, @"(((5 + 6) * 9) / 20.5) - sqrt(2)", nil);
+    
 }
 
 @end
